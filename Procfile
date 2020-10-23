eb01a4cd713f97cc46e5a3c165effa5a18fb7890
Lotfi FATHI <lotfi.fathi@gmail.com>
2020-10-23 03:32:37 +0200

    Add Glassfish-web.xml

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JAVA_OPTS -cp target/classes:target/dependency/* com.sdzee.tp.startup.Main
+web: sh target/bin/webapp
--- src/main/java/com/sdzee/tp/startup/Main.java
+++ src/main/java/com/sdzee/tp/startup/Main.java
@@ -6,6 +6,7 @@ import org.glassfish.embeddable.Deployer;
 import org.glassfish.embeddable.GlassFish;
 import org.glassfish.embeddable.GlassFishProperties;
 import org.glassfish.embeddable.GlassFishRuntime;
+import org.glassfish.embeddable.archive.ScatteredArchive;
 
 	public class Main {
 
@@ -19,18 +20,19 @@ import org.glassfish.embeddable.GlassFishRuntime;
 			GlassFish glassfish = GlassFishRuntime.bootstrap().newGlassFish(gfProps);
 			glassfish.start();
 			
-	  /*	File webRoot = new File("src/main/webapp");
-			File classRoot = new File("target", "classes"); */
+	     	File webRoot = new File("src/main/webapp");
+			File classRoot = new File("target", "classes"); 
 
 			Deployer deployer = glassfish.getDeployer();
-		/*	ScatteredArchive archive = new ScatteredArchive("hello", ScatteredArchive.Type.WAR, webRoot);
+			ScatteredArchive archive = new ScatteredArchive("hello", ScatteredArchive.Type.WAR, webRoot);
 			archive.addClassPath(classRoot);
-			deployer.deploy(archive.toURI(), "--name=hello", "--contextroot=/"); */		
+			deployer.deploy(archive.toURI());
+		/*	deployer.deploy(archive.toURI(), "--name=hello", "--contextroot=/"); 	*/
          
            
-	   /*	File file = new File("tp10-0.0.1-SNAPSHOT.war"); */
-			File file = new File("tp10.war");
-			deployer.deploy(file);
+		/* 	File file = new File("tp10-0.0.1-SNAPSHOT.war"); 
+
+			deployer.deploy(file); */
 			
 	     } 
 		
